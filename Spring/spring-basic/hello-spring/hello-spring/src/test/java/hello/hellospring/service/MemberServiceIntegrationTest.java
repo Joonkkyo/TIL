332f662b85ed615a80851406bb040a18da1ff10e
Joonkyo <jkseo50@naver.com>
2024-07-07 13:17:46 +0900

    JdbcTemplate 적용 및 test code 수정

--- Spring/spring-basic/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ Spring/spring-basic/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,36 +1,33 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
 
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void join() {
         //given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("joonkyo");
 
         //when
         Long saveId = memberService.join(member);
@@ -44,10 +41,10 @@ class MemberServiceTest {
     public void duplicatedMemberTest() {
         //given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("joonkyo");
 
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("joonkyo");
 
         //when
         memberService.join(member1);
