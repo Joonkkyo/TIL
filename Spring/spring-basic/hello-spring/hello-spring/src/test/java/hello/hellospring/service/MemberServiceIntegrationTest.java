dea51e577f398198e3d356ea14181c4b7dd6109b
Joonkyo <jkseo50@naver.com>
2024-07-08 01:18:39 +0900

    JPA 적용 및 코드 수정

--- Spring/spring-basic/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ Spring/spring-basic/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -2,9 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,15 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
 @Transactional
+@SpringBootTest
 class MemberServiceIntegrationTest {
 
     @Autowired
     MemberService memberService;
-
-    @Autowired
-    MemberRepository memberRepository;
 
     @Test
     void join() {
